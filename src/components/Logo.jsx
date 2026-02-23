d0441ce68a687d17d82e1b410c8b1f494eab800e
mauro-fe <mauro.neto@grupointegrado.br>
2026-02-23 19:54:21 -0300

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Logo({ className = "h-10" }) {
   return (
     <img
-      src="/img/logo4.png"
+      src="/img/logo5.png"
       alt="MF Digital - Mauro Felix"
       className={className}
     />
